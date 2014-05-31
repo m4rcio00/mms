c6ea9d6a09ee8f4de5d5b0e49c17950fceb18bd3
Marcio Santana <marcio.santtanna@gmail.com>
2014-05-31 15:10:18 -0300

    cpf

--- app/code/local/DeivisonArthur/OnepageCheckout/controllers/IndexController.php
+++ app/code/local/DeivisonArthur/OnepageCheckout/controllers/IndexController.php
@@ -144,6 +144,7 @@ class DeivisonArthur_OnepageCheckout_IndexController extends Mage_Checkout_Contr
         }
 
         //$session->clear();
+        $session->clear();
         $this->loadLayout();
         $this->_initLayoutMessages('checkout/session');
         Mage::dispatchEvent('checkout_onepage_controller_success_action', array('order_ids' => array($lastOrderId)));
